ed220848a751356a2afc8bd79b71f754f4a94be0
shivamgupta57121 <shivamgupta57121@gmail.com>
2021-10-18 00:37:27 +0530

    Signup Flow

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const express = require("express");
 const app = express();
 // post accept
 app.use(express.json());
+// Serve static files - folder designate
+app.use(express.static('public'))
 
 // function -> route path
 // frontend -> req -> /
@@ -17,33 +19,51 @@ app.use(express.json());
 // });
 
 // CRUD application
-let user = {
-};
+// let user = {
+// };
 
-function createUser(req, res) {
-    console.log("req.body", req.body);
-    user = req.body;
-    res.status(200).send("data received and user added");
+// function createUser(req, res) {
+//     console.log("req.body", req.body);
+//     user = req.body;
+//     res.status(200).send("data received and user added");
+// }
+// function getUser(req, res) {
+//     console.log("users");
+//     // for sending key value pair
+//     res.json(user);
+// }
+// function updateUser(req, res) {
+//     let obj = req.body;
+//     for (let key in obj) {
+//         user[key] = obj[key];
+//     }
+//     res.status(200).json(user);
+// }
+// function deleteUser(req, res) {
+//     user = {};
+//     res.status(200).json(user);
+// }
+// function getUserById(req, res) {
+//     console.log(req.params);
+//     res.status(200).send("Hello");
+// }
+
+// Database 
+let user = [];
+function signupUser(req, res) {
+    //user name, email, password
+    let { name, email, password } = req.body;
+    console.log("user", req.body);
+    user.push({
+        email, name, password
+    });
+    res.status(200).json({
+        message: "user created",
+        createdUser: req.body
+    })
 }
-function getUser(req, res) {
-    console.log("users");
-    // for sending key value pair
-    res.json(user);
-}
-function updateUser(req, res) {
-    let obj = req.body;
-    for (let key in obj) {
-        user[key] = obj[key];
-    }
-    res.status(200).json(user);
-}
-function deleteUser(req, res) {
-    user = {};
-    res.status(200).json(user);
-}
-function getUserById(req, res) {
-    console.log(req.params);
-    res.status(200).send("Hello");
+function loginUser(req, res) {
+
 }
 
 // // Post - giving data to server
@@ -58,18 +78,24 @@ function getUserById(req, res) {
 // app.get("/api/user/:id", getUserById);
 
 // Mounting in express
-const router = express.Router();
-app.use("/api/user", router); // Common Path
-router
-    .route("/")
-    .get(getUser)
-    .post(createUser)
-    .patch(updateUser)
-    .delete(deleteUser);
+// const userRouter = express.Router();
+const authRouter = express.Router();
+// app.use("/api/user", userRouter); // Common Path
+app.use("/api/auth", authRouter);
+// userRouter
+//     .route("/")
+//     .get(getUser)
+//     .post(createUser)
+//     .patch(updateUser)
+//     .delete(deleteUser);
 
-router
-    .route("/:id")
-    .get(getUserById);
+// userRouter
+//     .route("/:id")
+//     .get(getUserById);
+
+authRouter
+    .post("/signup", signupUser)
+    .post("/login", loginUser);
 
 // localhost:8080
 app.listen(8080, function () {
