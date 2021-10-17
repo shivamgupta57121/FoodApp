23b765f58a858e041728d6f100970f04f3b84e5b
shivamgupta57121 <shivamgupta57121@gmail.com>
2021-10-17 22:04:59 +0530

    REST API examples

--- app.js
+++ app.js
@@ -11,45 +11,65 @@ app.use(express.json());
 
 // function -> route path
 // frontend -> req -> /
-app.get("/", function (req, res) {
-    console.log("Hello from home page");
-    res.send("<h1>Hello from Backend</h1>");
-});
+// app.get("/", function (req, res) {
+//     console.log("Hello from home page");
+//     res.send("<h1>Hello from Backend</h1>");
+// });
 
 // CRUD application
 let user = {
 };
-// Post - giving data to server
-app.post("/user", function (req, res) {
+
+function createUser(req, res) {
     console.log("req.body", req.body);
     user = req.body;
     res.status(200).send("data received and user added");
-})
-// Get - getting data from server
-app.get("/user", function (req, res) {
+}
+function getUser(req, res) {
     console.log("users");
     // for sending key value pair
     res.json(user);
-})
-// Patch - update data
-app.patch("/user", function (req, res) {
+}
+function updateUser(req, res) {
     let obj = req.body;
     for (let key in obj) {
         user[key] = obj[key];
     }
     res.status(200).json(user);
-})
-// Delete - delete data
-app.delete("/user", function (req, res) {
+}
+function deleteUser(req, res) {
     user = {};
     res.status(200).json(user);
-})
-
-// Template Route
-app.get("/user/:id", function (req, res) {
+}
+function getUserById(req, res) {
     console.log(req.params);
     res.status(200).send("Hello");
-})
+}
+
+// // Post - giving data to server
+// app.post("/api/user", createUser);
+// // Get - getting data from server
+// app.get("/api/user", getUser);
+// // Patch - update data
+// app.patch("/api/user", updateUser);
+// // Delete - delete data
+// app.delete("/api/user", deleteUser);
+// // Template Route
+// app.get("/api/user/:id", getUserById);
+
+// Mounting in express
+const router = express.Router();
+app.use("/api/user", router); // Common Path
+router
+    .route("/")
+    .get(getUser)
+    .post(createUser)
+    .patch(updateUser)
+    .delete(deleteUser);
+
+router
+    .route("/:id")
+    .get(getUserById);
 
 // localhost:8080
 app.listen(8080, function () {
